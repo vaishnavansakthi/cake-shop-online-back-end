2ab7a970d9fc20f02f469771d6112d36f76a6a30
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2024-09-15 11:53:53 +0530

    cors issue fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('Cake Shop API')
